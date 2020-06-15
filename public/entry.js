d431c88a96037959e0737fc64ac2dd6c94db25ec
Conor O'Riordan <oriordac1@hawkmail.newpaltz.edu>
2020-06-15 15:02:34 -0400

    See specific pokemon entry with more info

--- public/entry.js
+++ public/entry.js
@@ -17,26 +17,58 @@ const container = document.getElementById('entry');
 const getPokemon = (id) => __awaiter(void 0, void 0, void 0, function* () {
     const data = yield fetch(`https://pokeapi.co/api/v2/pokemon/${id}`);
     const pokemon = yield data.json();
+    const pokemonHeight = pokemon.height;
+    const pokemonWeight = pokemon.weight;
     const pokemonType = pokemon.types
         .map((poke) => poke.type.name)
         .join(', ');
+    const pokemonAbility = pokemon.abilities
+        .map((poke) => poke.ability.name)
+        .join(', ');
+    const data2 = yield fetch(`${pokemon.species.url}`);
+    const flavor = yield data2.json();
+    const pokemonFlavor = flavor.flavor_text_entries[0].flavor_text
+        .replace(/\s+/g, ' ')
+        .trim();
+    console.log(flavor);
     const transformedPokemon = {
         id: pokemon.id,
         name: pokemon.name,
         image: `${pokemon.sprites.front_default}`,
-        type: pokemonType
+        shiny: `${pokemon.sprites.front_shiny}`,
+        type: pokemonType,
+        ability: pokemonAbility,
+        weight: pokemonWeight,
+        height: pokemonHeight,
+        flavor: pokemonFlavor
     };
     showPokemon(transformedPokemon);
 });
 const showPokemon = (pokemon) => {
     let output = `
         <div class="card">
+            <span class="return" onclick="returnHome()">Return</span>
             <span class="card--id">#${pokemon.id}</span>
+            <h1 class="card--name">${pokemon.name}</h1>  
             <img class="card--image" src=${pokemon.image} alt=${pokemon.name} />
-            <h1 class="card--name">${pokemon.name}</h1>
-            <span class="card--details">${pokemon.type}</span>
+            <img class="card--image" src=${pokemon.shiny} />
+            <hr>
+            <div class="container">
+                <div class="left--half">
+                    <div class="entry--details"><span style="color:white;">Type:</span> ${pokemon.type}</div>
+                    <div class="entry--details"><span style="color:white;">Abilities:</span> ${pokemon.ability}</div>
+                    <div class="entry--details"><span style="color:white;">Height:</span> ${pokemon.height} dm</div>
+                    <div class="entry--details"><span style="color:white;">Weight:</span> ${pokemon.weight} gm</div>
+                </div>
+                <div class="right--half">
+                    <div class="entry--details">${pokemon.flavor}</div>
+                </div>
+            </div>
         </div>
     `;
     container.innerHTML += output;
 };
 getPokemon(entryId);
+const returnHome = () => {
+    window.location.href = `./index.html`;
+};
--- src/entry.ts
+++ src/entry.ts
@@ -12,33 +12,68 @@ interface IPokemon {
     id: number;
     name: string;
     image: string;
+    shiny: string;
     type: string;
+    ability: string;
+    weight: number;
+    height: number;
+    flavor: string;
 };
 
 const getPokemon = async (id: number): Promise<void> => {
     const data: Response = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`);
     const pokemon: any = await data.json();
+    const pokemonHeight: number = pokemon.height;
+    const pokemonWeight: number = pokemon.weight;
     const pokemonType: string = pokemon.types
         .map( (poke: any) => poke.type.name)
         .join(', ');
+    const pokemonAbility: string = pokemon.abilities
+        .map( (poke: any) => poke.ability.name)
+        .join(', ');
+
+    const data2: Response = await fetch(`${pokemon.species.url}`);
+    const flavor: any = await data2.json();
+    const pokemonFlavor: string = flavor.flavor_text_entries[0].flavor_text
+        .replace(/\s+/g, ' ')
+        .trim();
+    console.log(flavor);
     
     const transformedPokemon = {
         id: pokemon.id,
         name: pokemon.name,
         image: `${pokemon.sprites.front_default}`,
-        type: pokemonType
+        shiny: `${pokemon.sprites.front_shiny}`,
+        type: pokemonType,
+        ability: pokemonAbility,
+        weight: pokemonWeight,
+        height: pokemonHeight,
+        flavor: pokemonFlavor
     };
-
+    
     showPokemon(transformedPokemon);
 };
 
 const showPokemon = (pokemon: IPokemon): void => {
     let output: string = `
         <div class="card">
+            <span class="return" onclick="returnHome()">Return</span>
             <span class="card--id">#${pokemon.id}</span>
+            <h1 class="card--name">${pokemon.name}</h1>  
             <img class="card--image" src=${pokemon.image} alt=${pokemon.name} />
-            <h1 class="card--name">${pokemon.name}</h1>
-            <span class="card--details">${pokemon.type}</span>
+            <img class="card--image" src=${pokemon.shiny} />
+            <hr>
+            <div class="container">
+                <div class="left--half">
+                    <div class="entry--details"><span style="color:white;">Type:</span> ${pokemon.type}</div>
+                    <div class="entry--details"><span style="color:white;">Abilities:</span> ${pokemon.ability}</div>
+                    <div class="entry--details"><span style="color:white;">Height:</span> ${pokemon.height} dm</div>
+                    <div class="entry--details"><span style="color:white;">Weight:</span> ${pokemon.weight} gm</div>
+                </div>
+                <div class="right--half">
+                    <div class="entry--details">${pokemon.flavor}</div>
+                </div>
+            </div>
         </div>
     `;
 
@@ -46,3 +81,7 @@ const showPokemon = (pokemon: IPokemon): void => {
 };
 
 getPokemon(entryId);
+
+const returnHome = ( ) => {
+    window.location.href = `./index.html`;
+}
